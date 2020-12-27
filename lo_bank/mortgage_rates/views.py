d4c9943ea2c8b1100cb2cdf7d6669bf5427901ca
Chandra Yalangi <chandra@levelops.io>
2020-12-27 13:43:02 -0800

    Test security hotspots

--- lo_bank/mortgage_rates/views.py
+++ lo_bank/mortgage_rates/views.py
@@ -2,6 +2,39 @@ import tempfile
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
+import logging
+from logging import Logger, Handler, Filter
+from logging.config import fileConfig, dictConfig
+
+logging.basicConfig()  # Sensitive
+
+logging.disable()  # Sensitive
+
+
+def update_logging(logger_class):
+    logging.setLoggerClass(logger_class)  # Sensitive
+
+
+def set_last_resort(last_resort):
+    logging.lastResort = last_resort  # Sensitive
+
+
+class CustomLogger(Logger):  # Sensitive
+    pass
+
+
+class CustomHandler(Handler):  # Sensitive
+    pass
+
+
+class CustomFilter(Filter):  # Sensitive
+    pass
+
+
+def update_config(path, config):
+    fileConfig(path)  # Sensitive
+    dictConfig(config)  # Sensitive
+
 # Create your views here.
 
 # CWE-601 URL Redirection to Untrusted Site ('Open Redirect')
