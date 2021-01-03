aaee1041c8012e27df3588a6a2955b255e8b644f
bob-levelops <76456507+bob-levelops@users.noreply.github.com>
2021-01-02 18:58:00 -0800

    LB-6 Pyunit tests (#5)
    * #LB-6 Add unit tests to Django
    
    * LB-6 Update requirements.txt
    
    Co-authored-by: Chandra Yalangi <chandra@levelops.io>

--- lo_bank/bank/tests.py
+++ lo_bank/bank/tests.py
@@ -1,3 +1,50 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from .models import BankAccount, InvalidAmount
 
 # Create your tests here.
+class BankAccountTestCase(TestCase):
+    def setUp(self):
+        self.bob = get_user_model().objects.create()
+        self.bob_account = BankAccount.objects.create(name='Bob Checking', owner=self.bob)
+
+    def test_deposit_cash(self):
+        first_deposit = 60
+        self.bob_account.deposit_cash(first_deposit)
+        self.assertEqual(self.bob_account.get_balance(), first_deposit)
+        second_deposit = 30
+        self.bob_account.deposit_cash(second_deposit)
+        self.assertEqual(self.bob_account.get_balance(), first_deposit+second_deposit)
+
+    def test_deposit_check(self):
+        first_deposit = 60
+        self.bob_account.deposit_cash(first_deposit)
+        self.assertEqual(self.bob_account.get_balance(), first_deposit)
+        second_deposit = 30
+        self.bob_account.deposit_cash(second_deposit)
+        self.assertEqual(self.bob_account.get_balance(), first_deposit+second_deposit)
+
+    def test_deposit_wire(self):
+        first_deposit = 60
+        self.bob_account.deposit_cash(first_deposit)
+        self.assertEqual(self.bob_account.get_balance(), first_deposit)
+        second_deposit = 30
+        self.bob_account.deposit_cash(second_deposit)
+        self.assertEqual(self.bob_account.get_balance(), first_deposit+second_deposit)
+
+    def test_invalid_deposit(self):
+        with self.assertRaises(InvalidAmount):
+            self.bob_account.deposit_cash(-10)
+
+    def test_withdraw_cash(self):
+        first_deposit = 60
+        self.bob_account.deposit_cash(first_deposit)
+        first_withdraw = 30
+        self.bob_account.withdraw_cash(first_withdraw)
+        self.assertEqual(self.bob_account.get_balance(), first_deposit-first_withdraw)
+
+    def test_invalid_withdraw_amount(self):
+        first_deposit = 60
+        self.bob_account.deposit_cash(first_deposit)
+        with self.assertRaises(InvalidAmount):
+            self.bob_account.withdraw_cash(first_deposit+10)
