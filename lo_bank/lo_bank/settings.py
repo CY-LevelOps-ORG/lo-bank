10c3d5253a1bde7bb99ee064b613ba05b472dbd6
Chandra Yalangi <chandra@levelops.io>
2021-01-03 09:37:36 -0800

    Configure lo_bank to use postgres

--- lo_bank/lo_bank/settings.py
+++ lo_bank/lo_bank/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'lo_bank.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lo_bank',
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PWD'],
+        'HOST': os.environ['POSTGRES_HOST'],
     }
 }
 
